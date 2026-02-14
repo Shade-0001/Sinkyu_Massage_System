4b66759ee0e79520d4c2b161fbc78db3619bb648
Shade-0001 <ak74g3@gmail.com>
2026-02-14 13:39:08 +0900

    ［autosave-commit_260214-2233｜座標調整ツールのcustomTitleText動作確認用デバッグログ追加：読み込み・更新・送信の各段階でログ出力］

--- public/js/coordinate-adjuster_core.js
+++ public/js/coordinate-adjuster_core.js
@@ -503,6 +503,9 @@ function previewPdf() {
     requestBody.custom_title_text = customTitleText;
   }
 
+  console.log('[PDF Preview] customTitleText:', customTitleText);
+  console.log('[PDF Preview] requestBody.custom_title_text:', requestBody.custom_title_text);
+
   fetch('/prints/preview-pdf', {
     method: 'POST',
     headers: {
@@ -604,6 +607,9 @@ function loadCustomSampleData() {
   if (storedTitle) {
     customTitleText = storedTitle;
   }
+  console.log('[Load CustomSampleData] titleStorageKey:', titleStorageKey);
+  console.log('[Load CustomSampleData] storedTitle:', storedTitle);
+  console.log('[Load CustomSampleData] customTitleText:', customTitleText);
 
   // consent_dateのデフォルト値を設定
   if (!customSampleData.consent_date) {
@@ -643,6 +649,10 @@ function updateCustomTitleText(value) {
   // localStorageに保存
   const storageKey = 'customTitleText_' + currentPdfType;
   localStorage.setItem(storageKey, value);
+
+  console.log('[Update CustomTitleText] storageKey:', storageKey);
+  console.log('[Update CustomTitleText] value:', value);
+  console.log('[Update CustomTitleText] customTitleText:', customTitleText);
 
   // プレビューを自動更新
   autoPreview();
