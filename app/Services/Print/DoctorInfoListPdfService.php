3fdaea5cf20248e1834926868e80654b41458628
Shade-0001 <ak74g3@gmail.com>
2026-02-28 14:37:23 +0900

    ［autosave-commit_260228-2031｜氏名結合の全角スペースを半角スペースに統一（DoctorInfoList・TherapyDeadlineList）］

--- app/Services/Print/DoctorInfoListPdfService.php
+++ app/Services/Print/DoctorInfoListPdfService.php
@@ -135,7 +135,7 @@ class DoctorInfoListPdfService extends BasePdfService
   }
 
   /**
-   * 医師氏名フォーマット（姓と名を全角スペースで結合）
+   * 医師氏名フォーマット（姓と名を半角スペースで結合）
    */
   protected function formatDoctorName(?string $lastName, ?string $firstName): string
   {
@@ -150,7 +150,7 @@ class DoctorInfoListPdfService extends BasePdfService
     if ($first === '') {
       return $last;
     }
-    return $last . "\u{3000}" . $first;
+    return $last . ' ' . $first;
   }
 
   /**
