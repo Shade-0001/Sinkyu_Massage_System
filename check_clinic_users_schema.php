ae972174b4b275d0a0a38851e9182cd2be719a49
Shade-0001 <ak74g3@gmail.com>
2026-03-03 17:35:49 +0900

    [autosave-commit_260303-1752|ルートの一時・デバッグファイルを一括削除]

--- check_clinic_users_schema.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$cols = DB::connection('sinkyu_massage_system_db')->select("DESCRIBE clinic_users");
-foreach ($cols as $c) {
-  echo "{$c->Field}: {$c->Type}, Null={$c->Null}, Default=" . ($c->Default ?? 'NULL') . "\n";
-}
