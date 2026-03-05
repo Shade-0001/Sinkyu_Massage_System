e6e497011f384b88617886c8289d040c347dfb14
Shade-0001 <ak74g3@gmail.com>
2026-03-06 04:37:26 +0900

    [autosave-commit_260306-0437|index: 2〜7番目の各メニューにNerd Fontsアイコンを追加]

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,11 +14,11 @@
 
   <ul>
     <li><a href="{{ route('records.index') }}"><i class="nf nf-md-file_document"></i> 実績データ</a></li>
-    <li><a href="{{ route('reports.index') }}">報告書データ</a></li>
-    <li><a href="{{ route('schedules.index') }}">スケジュール</a></li>
-    <li><a href="{{ route('master.index') }}">マスター登録</a></li>
-    <li><a href="{{ route('prints.index') }}">印刷メニュー</a></li>
-    <li><a href="{{ route('therapy-periods.index') }}">要加療期間リスト</a></li>
-    <li><a href="{{ route('deposits.index') }}">入金管理</a></li>
+    <li><a href="{{ route('reports.index') }}"><i class="nf nf-md-chat_processing_outline"></i> 報告書データ</a></li>
+    <li><a href="{{ route('schedules.index') }}"><i class="nf nf-md-calendar_month_outline"></i> スケジュール</a></li>
+    <li><a href="{{ route('master.index') }}"><i class="nf nf-md-database_plus_outline"></i> マスター登録</a></li>
+    <li><a href="{{ route('prints.index') }}"><i class="nf nf-md-printer_outline"></i> 印刷メニュー</a></li>
+    <li><a href="{{ route('therapy-periods.index') }}"><i class="nf nf-fa-list"></i> 要加療期間リスト</a></li>
+    <li><a href="{{ route('deposits.index') }}"><i class="nf nf-fa-yen"></i> 入金管理</a></li>
   </ul>
 </x-app-layout>
