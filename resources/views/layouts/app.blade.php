ad4d319827d04689357e6216bd0de4db2e25db95
Shade-0001 <ak74g3@gmail.com>
2026-03-06 02:21:02 +0900

    [autosave-commit_260306-0221|Nerd Fonts WebfontのCSSをapp.blade.phpに追加]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,9 @@
 		<!-- Bootstrap CSS -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
+		<!-- Nerd Fonts (Webfont) -->
+		<link rel="stylesheet" href="https://www.nerdfonts.com/assets/css/webfont.css">
+
 		<!-- DataTables CSS -->
 		<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
 
