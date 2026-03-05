feadb06108b2ef99f472f18d731c74ad1902bcee
Shade-0001 <ak74g3@gmail.com>
2026-03-06 04:55:41 +0900

    [autosave-commit_260306-0455|index: アイコンに7色割り当て、テキストをtext-darkに設定]

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,12 +13,12 @@
   <br>
 
   <ul class="list-unstyled fs-1 ms-3">
-    <li class="mb-2"><a class="text-decoration-none" href="{{ route('records.index') }}"><i class="nf nf-fa-file_text_o"></i> 実績データ</a></li>
-    <li class="mb-2"><a class="text-decoration-none" href="{{ route('reports.index') }}"><i class="nf nf-md-message_reply_text_outline"></i> 報告書データ</a></li>
-    <li class="mb-2"><a class="text-decoration-none" href="{{ route('schedules.index') }}"><i class="nf nf-md-calendar_month_outline"></i> スケジュール</a></li>
-    <li class="mb-2"><a class="text-decoration-none" href="{{ route('master.index') }}"><i class="nf nf-fa-edit"></i> マスター登録</a></li>
-    <li class="mb-2"><a class="text-decoration-none" href="{{ route('prints.index') }}"><i class="nf nf-md-printer_outline"></i> 印刷メニュー</a></li>
-    <li class="mb-2"><a class="text-decoration-none" href="{{ route('therapy-periods.index') }}"><i class="nf nf-fa-list"></i> 要加療期間リスト</a></li>
-    <li class="mb-2"><a class="text-decoration-none" href="{{ route('deposits.index') }}"><i class="nf nf-fa-yen"></i> 入金管理</a></li>
+    <li class="mb-2"><a class="text-decoration-none" href="{{ route('records.index') }}"><i class="nf nf-fa-file_text_o" style="color:#e74c3c;"></i> <span class="text-dark">実績データ</span></a></li>
+    <li class="mb-2"><a class="text-decoration-none" href="{{ route('reports.index') }}"><i class="nf nf-md-message_reply_text_outline" style="color:#e67e22;"></i> <span class="text-dark">報告書データ</span></a></li>
+    <li class="mb-2"><a class="text-decoration-none" href="{{ route('schedules.index') }}"><i class="nf nf-md-calendar_month_outline" style="color:#f1c40f;"></i> <span class="text-dark">スケジュール</span></a></li>
+    <li class="mb-2"><a class="text-decoration-none" href="{{ route('master.index') }}"><i class="nf nf-fa-edit" style="color:#2ecc71;"></i> <span class="text-dark">マスター登録</span></a></li>
+    <li class="mb-2"><a class="text-decoration-none" href="{{ route('prints.index') }}"><i class="nf nf-md-printer_outline" style="color:#3498db;"></i> <span class="text-dark">印刷メニュー</span></a></li>
+    <li class="mb-2"><a class="text-decoration-none" href="{{ route('therapy-periods.index') }}"><i class="nf nf-fa-list" style="color:#1a6bb5;"></i> <span class="text-dark">要加療期間リスト</span></a></li>
+    <li class="mb-2"><a class="text-decoration-none" href="{{ route('deposits.index') }}"><i class="nf nf-fa-yen" style="color:#9b59b6;"></i> <span class="text-dark">入金管理</span></a></li>
   </ul>
 </x-app-layout>
