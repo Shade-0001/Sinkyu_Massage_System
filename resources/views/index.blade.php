9b7946f58de649733d135273b3afc12e04232336
Shade-0001 <ak74g3@gmail.com>
2026-03-06 04:40:18 +0900

    [autosave-commit_260306-0440|index: マスター登録アイコンをdatabase_plus_outline→note_edit_outlineに変更]

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
     <li><a href="{{ route('records.index') }}"><i class="nf nf-md-file_document_outline"></i> 実績データ</a></li>
     <li><a href="{{ route('reports.index') }}"><i class="nf nf-md-chat_processing_outline"></i> 報告書データ</a></li>
     <li><a href="{{ route('schedules.index') }}"><i class="nf nf-md-calendar_month_outline"></i> スケジュール</a></li>
-    <li><a href="{{ route('master.index') }}"><i class="nf nf-md-database_plus_outline"></i> マスター登録</a></li>
+    <li><a href="{{ route('master.index') }}"><i class="nf nf-md-note_edit_outline"></i> マスター登録</a></li>
     <li><a href="{{ route('prints.index') }}"><i class="nf nf-md-printer_outline"></i> 印刷メニュー</a></li>
     <li><a href="{{ route('therapy-periods.index') }}"><i class="nf nf-fa-list"></i> 要加療期間リスト</a></li>
     <li><a href="{{ route('deposits.index') }}"><i class="nf nf-fa-yen"></i> 入金管理</a></li>
