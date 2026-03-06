c1d46cf6a8698a56485ec0d7d712592e84fbb555
Shade-0001 <ak74g3@gmail.com>
2026-03-06 13:36:36 +0900

    ［autosave-commit_260306-1336｜サイドバーのアイコン色をindexの背景色に揃える］

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -18,17 +18,17 @@
   <nav class="p-0">
     <ul class="list-unstyled p-0 m-0">
       <li class="border-bottom border-secondary">
-        <a href="{{ route('records.index') }}" class="sidebar-link"><i class="nf nf-fa-file_text_o"></i> 実績データ</a>
+        <a href="{{ route('records.index') }}" class="sidebar-link"><i class="nf nf-fa-file_text_o" style="color:#e74c3c;"></i> 実績データ</a>
       </li>
       <li class="border-bottom border-secondary">
-        <a href="{{ route('reports.index') }}" class="sidebar-link"><i class="nf nf-md-message_reply_text_outline"></i> 報告書データ</a>
+        <a href="{{ route('reports.index') }}" class="sidebar-link"><i class="nf nf-md-message_reply_text_outline" style="color:#e67e22;"></i> 報告書データ</a>
       </li>
       <li class="border-bottom border-secondary">
-        <a href="{{ route('schedules.index') }}" class="sidebar-link"><i class="nf nf-md-calendar_month_outline"></i> スケジュール</a>
+        <a href="{{ route('schedules.index') }}" class="sidebar-link"><i class="nf nf-md-calendar_month_outline" style="color:#f1c40f;"></i> スケジュール</a>
       </li>
       <li class="border-bottom border-secondary">
         <div class="sidebar-link sidebar-submenu-toggle" data-target="master-submenu">
-          <span><i class="nf nf-fa-edit"></i> マスター登録</span>
+          <span><i class="nf nf-fa-edit" style="color:#8bc34a;"></i> マスター登録</span>
           <span class="submenu-arrow">▼</span>
         </div>
         <ul id="master-submenu" class="submenu">
@@ -45,7 +45,7 @@
       </li>
       <li class="border-bottom border-secondary">
         <div class="sidebar-link sidebar-submenu-toggle" data-target="submaster-submenu">
-          <span><i class="nf nf-fa-edit"></i> サブマスター登録</span>
+          <span><i class="nf nf-fa-edit" style="color:#27ae60;"></i> サブマスター登録</span>
           <span class="submenu-arrow">▼</span>
         </div>
         <ul id="submaster-submenu" class="submenu">
@@ -56,13 +56,13 @@
         </ul>
       </li>
       <li class="border-bottom border-secondary">
-        <a href="{{ route('prints.index') }}" class="sidebar-link"><i class="nf nf-md-printer_outline"></i> 印刷メニュー</a>
+        <a href="{{ route('prints.index') }}" class="sidebar-link"><i class="nf nf-md-printer_outline" style="color:#3498db;"></i> 印刷メニュー</a>
       </li>
       <li class="border-bottom border-secondary">
-        <a href="{{ route('therapy-periods.index') }}" class="sidebar-link"><i class="nf nf-fa-list"></i> 要加療期間リスト</a>
+        <a href="{{ route('therapy-periods.index') }}" class="sidebar-link"><i class="nf nf-fa-list" style="color:#1a6bb5;"></i> 要加療期間リスト</a>
       </li>
       <li class="border-bottom border-secondary">
-        <a href="{{ route('deposits.index') }}" class="sidebar-link"><i class="nf nf-fa-yen"></i> 入金管理</a>
+        <a href="{{ route('deposits.index') }}" class="sidebar-link"><i class="nf nf-fa-yen" style="color:#9b59b6;"></i> 入金管理</a>
       </li>
     </ul>
   </nav>
