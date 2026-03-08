26fb7aba187307dbbe84e6c0412431c9c336d4e7
Shade-0001 <ak74g3@gmail.com>
2026-03-08 23:09:17 +0900

    autosave-commit[260308-2309] [タスク内容：#home-menuのブレイクポイント切替をMutationObserver(JS)からCSS統一方式に変更]

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,27 +75,4 @@
     </div>
   </div>
 
-@push('scripts')
-<script>
-  // 〈 ホームメニューブレイクポイント切替スクリプト 〉
-  // サイドバー開閉状態に応じて#home-menuのクラスを切り替え
-  const homeMenu = document.getElementById('home-menu');
-
-  if (homeMenu) {
-    function updateHomeMenuLayout() {
-      const isOpen = document.documentElement.dataset.sidebar === 'open';
-      homeMenu.classList.toggle('flex-lg-row', !isOpen);
-      homeMenu.classList.toggle('flex-xl-row', isOpen);
-    }
-
-    updateHomeMenuLayout();
-
-    new MutationObserver(updateHomeMenuLayout).observe(
-      document.documentElement,
-      { attributeFilter: ['data-sidebar'] }
-    );
-  }
-</script>
-@endpush
-
 </x-app-layout>
