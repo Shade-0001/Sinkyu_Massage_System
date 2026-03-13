1365e4b2c3d5db13f2e8536c21e4e2d9e1c0e15c
Shade-0001 <ak74g3@gmail.com>
2026-03-13 15:19:35 +0900

    ［auto-commit_260313-1519｜〈CSS/JS〉btn-custom-ハイライト復活時に0.5sフェードイン実装（btn-hover-highlight-fadeinクラスで制御）］

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,15 +8,21 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-// btn-custom-blue: クリック終了時にホバーハイライトを一時的に無効化
+// btn-custom-blue: クリック終了時にホバーハイライトを一時的に無効化→フェードイン復活
 document.addEventListener('mouseup', (e) => {
   const btn = e.target.closest('.btn-custom-blue');
   if (!btn) return;
 
+  clearTimeout(btn._hoverHighlightTimer);
+  btn.classList.remove('btn-hover-highlight-fadein');
   btn.classList.add('btn-hover-highlight-off');
 
-  clearTimeout(btn._hoverHighlightTimer);
   btn._hoverHighlightTimer = setTimeout(() => {
     btn.classList.remove('btn-hover-highlight-off');
+    btn.classList.add('btn-hover-highlight-fadein');
+
+    setTimeout(() => {
+      btn.classList.remove('btn-hover-highlight-fadein');
+    }, 500);
   }, 1000);
 });
