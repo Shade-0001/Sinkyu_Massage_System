00cf0c63855b027fbea4a0a8c3adf464da8b2469
Shade-0001 <ak74g3@gmail.com>
2026-03-26 23:39:33 +0900

    ［normal-commit_260326-2339｜〈app.css〉コメント修正・空の.btn-custom-subサイズ定義を削除、〈records_index〉カレンダー幅250px→380px拡大・calendar-title-displayのpx-2削除・selectのpaddingスタイル削除、〈auto-commit_260326-1659〉通知ウィジェット：notice-panelsのpointer-eventsをパネル個別管理に変更しリストパネル展開時の枠外クリック格納バグを修正］

--- resources/views/records/records_index.blade.php
+++ resources/views/records/records_index.blade.php
@@ -49,14 +49,14 @@
 
     <div class="d-flex gap-3 align-items-start">
       <!-- カレンダー -->
-      <div class="text-center position-relative" style="width: 250px;">
+      <div class="text-center position-relative" style="width: 380px;">
         <div class="d-flex align-items-stretch justify-content-center mb-3">
           <button type="button" id="prev-month-btn" class="btn-custom btn-custom-sub btn-custom-blue" style="--btn-br-tl: 16px; --btn-br-tr: 0px; --btn-br-br: 0px; --btn-br-bl: 16px;">
             <i class="nf nf-fa-angle_left fs-4"></i>
           </button>
           <div class="btn-custom btn-custom-sub btn-custom-blue rounded-0 fs-4">
-            <div id="calendar-title-display" class="px-2"></div>
-            <select id="calendar-title" class="position-absolute top-0 start-50 translate-middle-x opacity-0" style="font-size: 1.5rem; padding: 0.2em 0em; border: none; background: transparent; width: 100%; height: 100%;"></select>
+            <div id="calendar-title-display"></div>
+            <select id="calendar-title" class="position-absolute top-0 start-50 translate-middle-x opacity-0" style="font-size: 1.5rem; border: none; background: transparent; width: 100%; height: 100%;"></select>
           </div>
           <button type="button" id="next-month-btn" class="btn-custom btn-custom-sub btn-custom-blue" style="--btn-br-tl: 0px; --btn-br-tr: 16px; --btn-br-br: 16px; --btn-br-bl: 0px;">
             <i class="nf nf-fa-angle_right fs-4"></i>
