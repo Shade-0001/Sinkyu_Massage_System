41fcdb6b9c5a47c179a153a1e5952df093a03300
Shade-0001 <ak74g3@gmail.com>
2026-04-01 14:57:03 +0900

    ［auto-commit_260401-1457｜admin-panelのファビコンをBuild Fill（Material Symbols）に変更、パンくずリストアイコンをnf-md-wrenchに変更］

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -148,7 +148,7 @@ Breadcrumbs::define('index', function() {
 function getAdminPanelBreadcrumbs() {
   return [
     ...getIndexBreadcrumbs(),
-    ['url' => route('admin-panel.index'), 'label' => '管理画面', 'icon' => 'nf nf-fa-shield'],
+    ['url' => route('admin-panel.index'), 'label' => '管理画面', 'icon' => 'nf nf-md-wrench'],
   ];
 }
 
