b006be73c30e8d5321a34290f676b2edf79a2684
Shade-0001 <ak74g3@gmail.com>
2026-04-03 11:37:17 +0900

    ［auto-commit_260403-1137｜MonthlySchedulePdfService：$timeTextの重複定義を解消］

--- app/Services/Print/MonthlySchedulePdfService.php
+++ app/Services/Print/MonthlySchedulePdfService.php
@@ -603,7 +603,6 @@ class MonthlySchedulePdfService extends BasePdfService
       $pdf->Cell($nameW, 0, $nameText, 0, 0, 'L', false);
       $pdf->SetFont('kozgopromedium', 'B', $fontSize);
     } else {
-      $timeText = $startText . ' - ' . $endText;
       $pdf->SetXY($eventX + $textPaddingX, $textStartY);
       $pdf->Cell($innerW, 0, $timeText, 0, 0, 'L', false);
 
