b47ac2e7c88afd1b39e6fde550eaf92f70f9c26c
Shade-0001 <ak74g3@gmail.com>
2026-04-09 13:41:34 +0900

    ［auto-commit_260409-1341｜clinic-users_form：ラベルをタブ形状（field-label-tab）スタイルに変更・utility-classes.cssにfield-label-tabクラスを追加］

--- resources/views/master/clinic-users/clinic-users_form.blade.php
+++ resources/views/master/clinic-users/clinic-users_form.blade.php
@@ -19,16 +19,15 @@
   <input type="hidden" name="id" value="{{ $clinicUser->id }}">
   @endif
 
-  <div class="d-flex flex-column gap-2" style="max-width: 600px;">
+  <div class="d-flex flex-column gap-3" style="max-width: 600px;">
 
     {{-- 氏名 --}}
-    <div class="border rounded-2">
-      <div class="px-3 small fw-semibold bg-gray-100">氏名
+    <div>
+      <div class="field-label-tab">氏名
         @error('last_name')<span class="text-danger ms-2">{{ $message }}</span>@enderror
         @error('first_name')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </div>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <div class="d-flex gap-2 align-items-center">
           <div>
             <label for="last_name" class="form-label small mb-1">姓</label>
@@ -43,13 +42,12 @@
     </div>
 
     {{-- フリガナ --}}
-    <div class="border rounded-1 overflow-hidden">
-      <div class="px-3 py-2 small fw-semibold bg-gray-100">フリガナ
+    <div>
+      <div class="field-label-tab">フリガナ
         @error('last_kana')<span class="text-danger ms-2">{{ $message }}</span>@enderror
         @error('first_kana')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </div>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <div class="d-flex gap-2 align-items-center">
           <div>
             <label for="last_kana" class="form-label small mb-1">セイ</label>
@@ -64,30 +62,27 @@
     </div>
 
     {{-- 生年月日 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="birthday">生年月日</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="birthday">生年月日</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="date" id="birthday" name="birthday" value="{{ old('birthday', session($sessionKey . '.birthday', isset($clinicUser) && !empty($clinicUser->birthday) ? ($clinicUser->birthday instanceof \Carbon\Carbon ? $clinicUser->birthday->format('Y-m-d') : $clinicUser->birthday) : '')) }}">
       </div>
     </div>
 
     {{-- 年齢 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="age">年齢
+    <div>
+      <label class="field-label-tab" for="age">年齢
         @error('age')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="number" id="age" name="age" value="{{ $get('age') }}" min="0" max="150" readonly style="cursor: default;" data-tooltip="生年月日から自動入力されます">
       </div>
     </div>
 
     {{-- 性別 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="gender_id">性別</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="gender_id">性別</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         @php $genderVal = old('gender_id', session($sessionKey . '.gender_id', isset($clinicUser) ? $clinicUser->gender_id ?? '' : '')); @endphp
         <select id="gender_id" name="gender_id">
           <option value="">╌╌╌</option>
@@ -98,129 +93,116 @@
     </div>
 
     {{-- 郵便番号 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="postal_code">郵便番号
+    <div>
+      <label class="field-label-tab" for="postal_code">郵便番号
         @error('postal_code')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="text" id="postal_code" name="postal_code" value="{{ $get('postal_code') }}" placeholder="000-0000" maxlength="8">
         <div id="address-message" class="loading d-none mt-1"></div>
       </div>
     </div>
 
     {{-- 都道府県 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="address_1">都道府県
+    <div>
+      <label class="field-label-tab" for="address_1">都道府県
         @error('address_1')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="text" id="address_1" name="address_1" value="{{ $get('address_1') }}" readonly data-tooltip="郵便番号から自動入力されます">
       </div>
     </div>
 
     {{-- 市区町村番地以下 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="address_2">市区町村番地以下
+    <div>
+      <label class="field-label-tab" for="address_2">市区町村番地以下
         @error('address_2')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="text" id="address_2" name="address_2" value="{{ $get('address_2') }}">
       </div>
     </div>
 
     {{-- アパート・マンション名等 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="address_3">アパート・マンション名等
+    <div>
+      <label class="field-label-tab" for="address_3">アパート・マンション名等
         @error('address_3')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="text" id="address_3" name="address_3" value="{{ $get('address_3') }}">
       </div>
     </div>
 
     {{-- 電話番号 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="phone">電話番号</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="phone">電話番号</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="text" id="phone" name="phone" value="{{ $get('phone') }}">
       </div>
     </div>
 
     {{-- 携帯番号 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="cell_phone">携帯番号</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="cell_phone">携帯番号</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="text" id="cell_phone" name="cell_phone" value="{{ $get('cell_phone') }}">
       </div>
     </div>
 
     {{-- FAX番号 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="fax">FAX番号</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="fax">FAX番号</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="text" id="fax" name="fax" value="{{ $get('fax') }}">
       </div>
     </div>
 
     {{-- メールアドレス --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="email">メールアドレス</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="email">メールアドレス</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="email" id="email" name="email" value="{{ $get('email') }}">
       </div>
     </div>
 
     {{-- 往診距離 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="housecall_distance">往診距離（合計）</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="housecall_distance">往診距離（合計）</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="number" id="housecall_distance" name="housecall_distance" value="{{ $get('housecall_distance') }}" min="0">
       </div>
     </div>
 
     {{-- 往診加算距離 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="housecall_additional_distance">往診加算距離</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="housecall_additional_distance">往診加算距離</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <div class="text-secondary small mb-1">2㎞を超える場合の加算距離です。上記往診距離が2㎞以上の場合自動で入力されます</div>
         <input type="number" id="housecall_additional_distance" name="housecall_additional_distance" value="{{ $get('housecall_additional_distance') }}" min="0">
       </div>
     </div>
 
     {{-- 償還対象 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="is_redeemed">償還対象</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="is_redeemed">償還対象</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         @php $redeemed = old('is_redeemed', session($sessionKey . '.is_redeemed', isset($clinicUser) ? $clinicUser->is_redeemed ?? '' : '')); @endphp
         <input type="checkbox" id="is_redeemed" name="is_redeemed" value="1" {{ $redeemed ? 'checked' : '' }}>
       </div>
     </div>
 
     {{-- 申請書提出開始回数 --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="application_count">申請書提出開始回数<span class="fw-normal ms-1">［大阪市のみ］</span></label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="application_count">申請書提出開始回数<span class="fw-normal ms-1">［大阪市のみ］</span></label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <input type="number" id="application_count" name="application_count" value="{{ $get('application_count') }}" min="0">
       </div>
     </div>
 
     {{-- メモ --}}
-    <div class="border rounded-1 overflow-hidden">
-      <label class="d-block px-3 py-2 small fw-semibold bg-gray-100" for="note">メモ</label>
-      <hr class="m-0">
-      <div class="px-3 py-2 bg-gray-96">
+    <div>
+      <label class="field-label-tab" for="note">メモ</label>
+      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
         <textarea id="note" name="note" rows="4" style="width: 100%;">{{ $get('note') }}</textarea>
       </div>
     </div>
