ba4c7e31b9a6e42b6f50326134b6cc3ad501eb24
Shade-0001 <ak74g3@gmail.com>
2026-04-09 15:06:57 +0900

    ［auto-commit_260409-1506｜往診加算距離フィールド：テキスト説明をreadonly＋data-tooltipに変更（年齢フィールドと同パターン）］

--- resources/views/master/clinic-users/clinic-users_form.blade.php
+++ resources/views/master/clinic-users/clinic-users_form.blade.php
@@ -27,7 +27,8 @@
         @error('last_name')<span class="text-danger ms-2">{{ $message }}</span>@enderror
         @error('first_name')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </div>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <div class="d-flex gap-2 align-items-center">
           <div>
             <label for="last_name" class="form-label small mb-1">姓</label>
@@ -47,7 +48,8 @@
         @error('last_kana')<span class="text-danger ms-2">{{ $message }}</span>@enderror
         @error('first_kana')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </div>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <div class="d-flex gap-2 align-items-center">
           <div>
             <label for="last_kana" class="form-label small mb-1">セイ</label>
@@ -64,7 +66,8 @@
     {{-- 生年月日 --}}
     <div>
       <label class="form-label-tab" for="birthday">生年月日</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="date" id="birthday" name="birthday" value="{{ old('birthday', session($sessionKey . '.birthday', isset($clinicUser) && !empty($clinicUser->birthday) ? ($clinicUser->birthday instanceof \Carbon\Carbon ? $clinicUser->birthday->format('Y-m-d') : $clinicUser->birthday) : '')) }}">
       </div>
     </div>
@@ -74,7 +77,8 @@
       <label class="form-label-tab" for="age">年齢
         @error('age')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="number" id="age" name="age" value="{{ $get('age') }}" min="0" max="150" readonly style="cursor: default;" data-tooltip="生年月日から自動入力されます">
       </div>
     </div>
@@ -82,7 +86,8 @@
     {{-- 性別 --}}
     <div>
       <label class="form-label-tab" for="gender_id">性別</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         @php $genderVal = old('gender_id', session($sessionKey . '.gender_id', isset($clinicUser) ? $clinicUser->gender_id ?? '' : '')); @endphp
         <select id="gender_id" name="gender_id">
           <option value="">╌╌╌</option>
@@ -97,7 +102,8 @@
       <label class="form-label-tab" for="postal_code">郵便番号
         @error('postal_code')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="text" id="postal_code" name="postal_code" value="{{ $get('postal_code') }}" placeholder="000-0000" maxlength="8">
         <div id="address-message" class="loading d-none mt-1"></div>
       </div>
@@ -108,7 +114,8 @@
       <label class="form-label-tab" for="address_1">都道府県
         @error('address_1')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="text" id="address_1" name="address_1" value="{{ $get('address_1') }}" readonly data-tooltip="郵便番号から自動入力されます">
       </div>
     </div>
@@ -118,7 +125,8 @@
       <label class="form-label-tab" for="address_2">市区町村番地以下
         @error('address_2')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="text" id="address_2" name="address_2" value="{{ $get('address_2') }}">
       </div>
     </div>
@@ -128,7 +136,8 @@
       <label class="form-label-tab" for="address_3">アパート・マンション名等
         @error('address_3')<span class="text-danger ms-2">{{ $message }}</span>@enderror
       </label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="text" id="address_3" name="address_3" value="{{ $get('address_3') }}">
       </div>
     </div>
@@ -136,7 +145,8 @@
     {{-- 電話番号 --}}
     <div>
       <label class="form-label-tab" for="phone">電話番号</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="text" id="phone" name="phone" value="{{ $get('phone') }}">
       </div>
     </div>
@@ -144,7 +154,8 @@
     {{-- 携帯番号 --}}
     <div>
       <label class="form-label-tab" for="cell_phone">携帯番号</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="text" id="cell_phone" name="cell_phone" value="{{ $get('cell_phone') }}">
       </div>
     </div>
@@ -152,7 +163,8 @@
     {{-- FAX番号 --}}
     <div>
       <label class="form-label-tab" for="fax">FAX番号</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="text" id="fax" name="fax" value="{{ $get('fax') }}">
       </div>
     </div>
@@ -160,7 +172,8 @@
     {{-- メールアドレス --}}
     <div>
       <label class="form-label-tab" for="email">メールアドレス</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="email" id="email" name="email" value="{{ $get('email') }}">
       </div>
     </div>
@@ -168,7 +181,8 @@
     {{-- 往診距離 --}}
     <div>
       <label class="form-label-tab" for="housecall_distance">往診距離（合計）</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="number" id="housecall_distance" name="housecall_distance" value="{{ $get('housecall_distance') }}" min="0">
       </div>
     </div>
@@ -176,16 +190,17 @@
     {{-- 往診加算距離 --}}
     <div>
       <label class="form-label-tab" for="housecall_additional_distance">往診加算距離</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
-        <div class="text-secondary small mb-1">2㎞を超える場合の加算距離です。上記往診距離が2㎞以上の場合自動で入力されます</div>
-        <input type="number" id="housecall_additional_distance" name="housecall_additional_distance" value="{{ $get('housecall_additional_distance') }}" min="0">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
+        <input type="number" id="housecall_additional_distance" name="housecall_additional_distance" value="{{ $get('housecall_additional_distance') }}" min="0" readonly style="cursor: default;" data-tooltip="往診距離から自動入力されます">
       </div>
     </div>
 
     {{-- 償還対象 --}}
     <div>
       <label class="form-label-tab" for="is_redeemed">償還対象</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         @php $redeemed = old('is_redeemed', session($sessionKey . '.is_redeemed', isset($clinicUser) ? $clinicUser->is_redeemed ?? '' : '')); @endphp
         <input type="checkbox" id="is_redeemed" name="is_redeemed" value="1" {{ $redeemed ? 'checked' : '' }}>
       </div>
@@ -194,7 +209,8 @@
     {{-- 申請書提出開始回数 --}}
     <div>
       <label class="form-label-tab" for="application_count">申請書提出開始回数<span class="fw-normal ms-1">［大阪市のみ］</span></label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <input type="number" id="application_count" name="application_count" value="{{ $get('application_count') }}" min="0">
       </div>
     </div>
@@ -202,7 +218,8 @@
     {{-- メモ --}}
     <div>
       <label class="form-label-tab" for="note">メモ</label>
-      <div class="border rounded-1 overflow-hidden bg-gray-96 px-3 py-2">
+      <div class="form-field px-3 py-2">
+        <div class="form-field-top"></div>
         <textarea id="note" name="note" rows="4" style="width: 100%;">{{ $get('note') }}</textarea>
       </div>
     </div>
