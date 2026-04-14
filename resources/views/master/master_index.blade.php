42052f9981c71863a0be7fc668869c7ed879c9c0
Shade-0001 <ak74g3@gmail.com>
2026-04-14 10:56:23 +0900

    ［auto-commit_260414-1056｜master_index：ナビリンクをrow g-2 + col-12のボタンレイアウトに変更（btn-ex-main btn-ex-blue、1列）］

--- resources/views/master/master_index.blade.php
+++ resources/views/master/master_index.blade.php
@@ -10,13 +10,15 @@
   />
   
   
-  <a href="{{ route('clinic-users.index') }}">利用者</a><br>
-  <a href="{{ route('doctors.index') }}">医師</a><br>
-  <a href="{{ route('therapists.index') }}">施術者</a><br>
-  <a href="{{ route('caremanagers.index') }}">ケアマネ</a><br>
-  <a href="{{ route('clinic-info.index') }}">自社情報</a><br>
-  <a href="{{ route('master.documents.index') }}">文面編集</a><br>
-  <a href="{{ route('master.treatment-fees.index') }}">施術料金</a><br>
-  <a href="{{ route('master.self-fees.index') }}">自費施術料金</a><br>
-  <a href="{{ route('master.document-association.index') }}">登録済み標準文書の確認･関連付け</a>
+  <div class="row g-2">
+    <div class="col-12"><a href="{{ route('clinic-users.index') }}" class="btn-ex-main btn-ex-blue w-100 text-nowrap">利用者</a></div>
+    <div class="col-12"><a href="{{ route('doctors.index') }}" class="btn-ex-main btn-ex-blue w-100 text-nowrap">医師</a></div>
+    <div class="col-12"><a href="{{ route('therapists.index') }}" class="btn-ex-main btn-ex-blue w-100 text-nowrap">施術者</a></div>
+    <div class="col-12"><a href="{{ route('caremanagers.index') }}" class="btn-ex-main btn-ex-blue w-100 text-nowrap">ケアマネ</a></div>
+    <div class="col-12"><a href="{{ route('clinic-info.index') }}" class="btn-ex-main btn-ex-blue w-100 text-nowrap">自社情報</a></div>
+    <div class="col-12"><a href="{{ route('master.documents.index') }}" class="btn-ex-main btn-ex-blue w-100 text-nowrap">文面編集</a></div>
+    <div class="col-12"><a href="{{ route('master.treatment-fees.index') }}" class="btn-ex-main btn-ex-blue w-100 text-nowrap">施術料金</a></div>
+    <div class="col-12"><a href="{{ route('master.self-fees.index') }}" class="btn-ex-main btn-ex-blue w-100 text-nowrap">自費施術料金</a></div>
+    <div class="col-12"><a href="{{ route('master.document-association.index') }}" class="btn-ex-main btn-ex-blue w-100 text-nowrap">登録済み標準文書の確認･関連付け</a></div>
+  </div>
 </x-app-layout>
