3e603095384e6c8c4c6dd47292c9bfc87c601527
Shade-0001 <ak74g3@gmail.com>
2026-04-14 14:42:19 +0900

    ［auto-commit_260414-1442｜master_index：医師・施術者・ケアマネ・自社情報・文面編集・施術料金・自費施術料金・標準文書ボタンをbtn-ex-skin構造に統一］

--- resources/views/master/master_index.blade.php
+++ resources/views/master/master_index.blade.php
@@ -13,17 +13,75 @@
   <div class="row g-3 m-3" style="max-width: 30%;">
     <div class="col-12">
       <a href="{{ route('clinic-users.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-blue btn-ex-rounded-full"><i class="nf nf-fa-users"></i></div>
-        <div class="btn-ex-skin btn-ex-blue btn-ex-rounded-full">利用者（{{ $clinicUserCount }}件）</div>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full">
+          <i class="nf nf-fa-user"></i>
+        </div>
+        <div>利用者（{{ $clinicUserCount }}件）</div>
       </a>
     </div>
-    <div class="col-12"><a href="{{ route('doctors.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full"><i class="nf nf-fa-stethoscope"></i><span class="vr"></span>医師（{{ $doctorCount }}件）</a></div>
-    <div class="col-12"><a href="{{ route('therapists.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full"><i class="nf nf-md-hand_heart"></i><span class="vr"></span>施術者（{{ $therapistCount }}件）</a></div>
-    <div class="col-12"><a href="{{ route('caremanagers.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full"><i class="nf nf-fa-id_card"></i><span class="vr"></span>ケアマネ（{{ $careManagerCount }}件）</a></div>
-    <div class="col-12"><a href="{{ route('clinic-info.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full"><i class="nf nf-fa-building"></i><span class="vr"></span>自社情報</a></div>
-    <div class="col-12"><a href="{{ route('master.documents.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full"><i class="nf nf-fa-file_text"></i><span class="vr"></span>文面編集</a></div>
-    <div class="col-12"><a href="{{ route('master.treatment-fees.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full"><i class="nf nf-fa-yen"></i><span class="vr"></span>施術料金</a></div>
-    <div class="col-12"><a href="{{ route('master.self-fees.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full"><i class="nf nf-fa-credit_card"></i><span class="vr"></span>自費施術料金</a></div>
-    <div class="col-12"><a href="{{ route('master.document-association.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full btn-ex-text-wrap"><i class="nf nf-fa-link"></i><span class="vr"></span>登録済み標準文書の確認･関連付け</a></div>
+    <div class="col-12">
+      <a href="{{ route('doctors.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full">
+          <i class="nf nf-fa-stethoscope"></i>
+        </div>
+        <div>医師（{{ $doctorCount }}件）</div>
+      </a>
+    </div>
+    <div class="col-12">
+      <a href="{{ route('therapists.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full">
+          <i class="nf nf-md-hand_heart"></i>
+        </div>
+        <div>施術者（{{ $therapistCount }}件）</div>
+      </a>
+    </div>
+    <div class="col-12">
+      <a href="{{ route('caremanagers.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full">
+          <i class="nf nf-fa-id_card"></i>
+        </div>
+        <div>ケアマネ（{{ $careManagerCount }}件）</div>
+      </a>
+    </div>
+    <div class="col-12">
+      <a href="{{ route('clinic-info.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full">
+          <i class="nf nf-fa-building"></i>
+        </div>
+        <div>自社情報</div>
+      </a>
+    </div>
+    <div class="col-12">
+      <a href="{{ route('master.documents.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full">
+          <i class="nf nf-fa-file_text"></i>
+        </div>
+        <div>文面編集</div>
+      </a>
+    </div>
+    <div class="col-12">
+      <a href="{{ route('master.treatment-fees.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full">
+          <i class="nf nf-fa-yen"></i>
+        </div>
+        <div>施術料金</div>
+      </a>
+    </div>
+    <div class="col-12">
+      <a href="{{ route('master.self-fees.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full">
+          <i class="nf nf-fa-credit_card"></i>
+        </div>
+        <div>自費施術料金</div>
+      </a>
+    </div>
+    <div class="col-12">
+      <a href="{{ route('master.document-association.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full btn-ex-text-wrap">
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full">
+          <i class="nf nf-fa-link"></i>
+        </div>
+        <div>登録済み標準文書の確認･関連付け</div>
+      </a>
+    </div>
   </div>
 </x-app-layout>
