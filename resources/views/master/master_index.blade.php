75555ad333fa0d2c4098d3bee3deebbeb930f40b
Shade-0001 <ak74g3@gmail.com>
2026-04-14 16:13:52 +0900

    ［auto-commit_260414-1613｜master_index：全ボタンを利用者ボタンの最新フォーマット（aspect-square m-n05・fs-7 m-n3・fs-5-5 ms-4 me-3）に統一］

--- resources/views/master/master_index.blade.php
+++ resources/views/master/master_index.blade.php
@@ -14,74 +14,74 @@
     <div class="col-12 btn-ex-main btn-ex-blue">ボタン</div>
     <div class="col-12">
       <a href="{{ route('clinic-users.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square" style="padding: 0.9rem; margin: -0.125rem;">
-          <i class="nf nf-fa-user fs-7-5 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
+          <i class="nf nf-fa-user fs-7 m-n3"></i>
         </div>
-        <div class="ms-4 me-3">利用者（{{ $clinicUserCount }}件）</div>
+        <div class="fs-5-5 ms-4 me-3">利用者（{{ $clinicUserCount }}件）</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('doctors.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square" style="padding: 0.9rem; margin: -0.125rem;">
-          <i class="nf nf-fa-user fs-7-5 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
+          <i class="nf nf-fa-user fs-7 m-n3"></i>
         </div>
-        <div class="ms-4 me-3">医師（{{ $doctorCount }}件）</div>
+        <div class="fs-5-5 ms-4 me-3">医師（{{ $doctorCount }}件）</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('therapists.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square" style="padding: 0.9rem; margin: -0.125rem;">
-          <i class="nf nf-fa-user fs-7-5 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
+          <i class="nf nf-fa-user fs-7 m-n3"></i>
         </div>
-        <div class="ms-4 me-3">施術者（{{ $therapistCount }}件）</div>
+        <div class="fs-5-5 ms-4 me-3">施術者（{{ $therapistCount }}件）</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('caremanagers.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square" style="padding: 0.9rem; margin: -0.125rem;">
-          <i class="nf nf-fa-user fs-7-5 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
+          <i class="nf nf-fa-user fs-7 m-n3"></i>
         </div>
-        <div class="ms-4 me-3">ケアマネ（{{ $careManagerCount }}件）</div>
+        <div class="fs-5-5 ms-4 me-3">ケアマネ（{{ $careManagerCount }}件）</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('clinic-info.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square" style="padding: 0.9rem; margin: -0.125rem;">
-          <i class="nf nf-fa-building fs-7-5 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
+          <i class="nf nf-fa-building fs-7 m-n3"></i>
         </div>
-        <div class="ms-4 me-3">自社情報</div>
+        <div class="fs-5-5 ms-4 me-3">自社情報</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('master.documents.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square" style="padding: 0.9rem; margin: -0.125rem;">
-          <i class="nf nf-md-file_document fs-7-5 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
+          <i class="nf nf-md-file_document fs-7 m-n3"></i>
         </div>
-        <div class="ms-4 me-3">文面編集</div>
+        <div class="fs-5-5 ms-4 me-3">文面編集</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('master.treatment-fees.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square" style="padding: 0.9rem; margin: -0.125rem;">
-          <i class="nf nf-fa-yen fs-7-5 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
+          <i class="nf nf-fa-yen fs-7 m-n3"></i>
         </div>
-        <div class="ms-4 me-3">施術料金</div>
+        <div class="fs-5-5 ms-4 me-3">施術料金</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('master.self-fees.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square" style="padding: 0.9rem; margin: -0.125rem;">
-          <i class="nf nf-fa-yen fs-7-5 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
+          <i class="nf nf-fa-yen fs-7 m-n3"></i>
         </div>
-        <div class="ms-4 me-3">自費施術料金</div>
+        <div class="fs-5-5 ms-4 me-3">自費施術料金</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('master.document-association.index') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square" style="padding: 0.9rem; margin: -0.125rem;">
-          <i class="nf nf-oct-link fs-7-5 fw-bold m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
+          <i class="nf nf-oct-link fs-7 fw-bold m-n3"></i>
         </div>
-        <div class="ms-4 me-3">登録済み標準文書の確認･関連付け</div>
+        <div class="fs-5-5 ms-4 me-3">登録済み標準文書の確認･関連付け</div>
       </a>
     </div>
   </div>
