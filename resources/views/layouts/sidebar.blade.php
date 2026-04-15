7caaeddbdeeb7e72e03e777ee4b5a199b191c428
Shade-0001 <ak74g3@gmail.com>
2026-04-15 14:54:19 +0900

    ［auto-commit_260415-1454｜サイドバー：トグルdivのtext-nowrapをa要素に移動・overflow-hiddenを追加してchevronのはみ出し防止］

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -62,8 +62,8 @@
         </a>
       </li>
       <li class="border-bottom border-1 border-secondary">
-        <div class="sidebar-link text-gray-90 {{ $onMaster ? 'sidebar-active' : '' }} fw-medium user-select-none d-flex align-items-center text-nowrap">
-          <a href="{{ route('master.index') }}" class="text-gray-90 text-decoration-none {{ $onMaster ? '' : 'hover-highlight-30 highlight-target-text-white' }} p-3 flex-grow-1 min-w-0"><i class="nf nf-fa-gear me-2 fs-5-5"></i>マスター登録</a>
+        <div class="sidebar-link text-gray-90 {{ $onMaster ? 'sidebar-active' : '' }} fw-medium user-select-none d-flex align-items-center overflow-hidden">
+          <a href="{{ route('master.index') }}" class="text-gray-90 text-decoration-none {{ $onMaster ? '' : 'hover-highlight-30 highlight-target-text-white' }} p-3 flex-grow-1 min-w-0 text-nowrap"><i class="nf nf-fa-gear me-2 fs-5-5"></i>マスター登録</a>
           <span class="sidebar-submenu-toggle flex-shrink-0 {{ $onMaster ? '' : 'hover-highlight-30 highlight-target-text-white' }} p-3" data-target="master-submenu">
             <i class="nf nf-md-chevron_down fs-7 submenu-arrow"></i>
           </span>
@@ -144,8 +144,8 @@
         </ul>
       </li>
       <li class="border-bottom border-1 border-secondary">
-        <div class="sidebar-link text-gray-85 {{ $onSubmaster ? 'sidebar-active' : '' }} fw-medium user-select-none d-flex align-items-center text-nowrap">
-          <a href="{{ route('submaster.index') }}" class="text-gray-85 text-decoration-none {{ $onSubmaster ? '' : 'hover-highlight-30 highlight-target-text-white' }} p-3 flex-grow-1 min-w-0">
+        <div class="sidebar-link text-gray-85 {{ $onSubmaster ? 'sidebar-active' : '' }} fw-medium user-select-none d-flex align-items-center overflow-hidden">
+          <a href="{{ route('submaster.index') }}" class="text-gray-85 text-decoration-none {{ $onSubmaster ? '' : 'hover-highlight-30 highlight-target-text-white' }} p-3 flex-grow-1 min-w-0 text-nowrap">
             <i class="nf nf-fa-gear me-2 fs-5-5"></i>サブマスター登録
           </a>
           <span class="sidebar-submenu-toggle flex-shrink-0 {{ $onSubmaster ? '' : 'hover-highlight-30 highlight-target-text-white' }} p-3" data-target="submaster-submenu">
@@ -204,8 +204,8 @@
       </li>
       @if(Auth::user()->is_admin)
       <li class="border-bottom border-1 border-secondary">
-        <div class="sidebar-link text-gray-85 {{ $onAdminPanel ? 'sidebar-active' : '' }} fw-medium user-select-none d-flex align-items-center text-nowrap">
-          <a href="{{ route('admin-panel.index') }}" class="text-gray-85 text-decoration-none {{ $onAdminPanel ? '' : 'hover-highlight-30 highlight-target-text-white' }} p-3 flex-grow-1 min-w-0">
+        <div class="sidebar-link text-gray-85 {{ $onAdminPanel ? 'sidebar-active' : '' }} fw-medium user-select-none d-flex align-items-center overflow-hidden">
+          <a href="{{ route('admin-panel.index') }}" class="text-gray-85 text-decoration-none {{ $onAdminPanel ? '' : 'hover-highlight-30 highlight-target-text-white' }} p-3 flex-grow-1 min-w-0 text-nowrap">
             <i class="nf nf-md-wrench me-2 fs-5-5"></i>システム管理
           </a>
           <span class="sidebar-submenu-toggle flex-shrink-0 {{ $onAdminPanel ? '' : 'hover-highlight-30 highlight-target-text-white' }} p-3" data-target="admin-panel-submenu">
