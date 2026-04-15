81c55baab61d86b20692b77108fd2fdc78a97ef3
Shade-0001 <ak74g3@gmail.com>
2026-04-15 16:24:21 +0900

    ［auto-commit_260415-1624｜reports_form：edit時のボタンラベルを「更新」・duplicate時を「複製登録」に変更］

--- resources/views/reports/reports_form.blade.php
+++ resources/views/reports/reports_form.blade.php
@@ -114,7 +114,12 @@
 
       <div class="mt-4 d-flex gap-2 justify-content-end">
         <a href="{{ route('reports.index', ['clinic_user_id' => $clinicUserId, 'scroll_year' => $year, 'scroll_month' => $month]) }}" class="btn-ex-main btn-ex-gray"><i class="nf nf-fa-caret_left me-1" style="transform: scale(1.2)"></i>戻る</a>
-        <button type="submit" class="btn-ex-main btn-ex-green">登録</button>
+        <button type="submit" class="btn-ex-main btn-ex-green">
+          @if($mode === 'edit') 更新
+          @elseif($mode === 'duplicate') 複製登録
+          @else 登録
+          @endif
+        </button>
       </div>
 
     </div>
