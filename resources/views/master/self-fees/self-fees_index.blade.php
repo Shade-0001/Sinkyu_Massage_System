47893d57e3cb0615b70fdbf05e479e2cc6555910
Shade-0001 <ak74g3@gmail.com>
2026-04-15 16:16:10 +0900

    ［auto-commit_260415-1616｜操作列幅修正：autoWidth: false削除・widthをmax-widthに戻す（max-width: 140px）］

--- resources/views/master/self-fees/self-fees_index.blade.php
+++ resources/views/master/self-fees/self-fees_index.blade.php
@@ -12,7 +12,7 @@
         <th class="text-center" style="width: 10%;">ID</th>
         <th class="text-center" style="width: 50%;">名称</th>
         <th class="text-center" style="width: 15%;">金額（円）</th>
-        <th class="text-center" style="width: 140px;">操作</th>
+        <th class="text-center" style="max-width: 140px;">操作</th>
       </tr>
     </thead>
     <tbody>
@@ -66,7 +66,6 @@
       
       // DataTables 初期化（定義ファイル：resources/js/app.js）
       var table = initDataTable('#selfPayFeesTable', {
-        autoWidth: false,
         order: [[0, 'desc']],
         columnDefs: [
           { orderable: false, targets: [3] }
