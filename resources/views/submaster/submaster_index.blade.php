72d467ba6b16197444a41144a301f59248c30fab
Shade-0001 <ak74g3@gmail.com>
2026-04-15 11:14:20 +0900

    ［auto-commit_260415-1114｜submaster_index：全ボタンのアイコンスキンクラスをbtn-ex-xs/p-3/fs-6/m-n2に統一］

--- resources/views/submaster/submaster_index.blade.php
+++ resources/views/submaster/submaster_index.blade.php
@@ -8,32 +8,32 @@
   <div class="row g-3 m-3">
     <div class="col-12">
       <a href="{{ route('submaster.medical-institutions') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
-          <i class="nf nf-fa-hospital fs-7 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square m-n05 p-3">
+          <i class="nf nf-fa-hospital fs-6 m-n2"></i>
         </div>
         <div class="fs-5-5 ms-4 me-3">医療機関名（{{ $counts['medical_institutions'] }}件）</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('submaster.service-providers') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
-          <i class="nf nf-fa-building fs-7 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square m-n05 p-3">
+          <i class="nf nf-fa-building fs-6 m-n2"></i>
         </div>
         <div class="fs-5-5 ms-4 me-3">サービス事業者名（{{ $counts['service_providers'] }}件）</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('submaster.conditions') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
-          <i class="nf nf-md-clipboard_pulse fs-7 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square m-n05 p-3">
+          <i class="nf nf-md-clipboard_pulse fs-6 m-n2"></i>
         </div>
         <div class="fs-5-5 ms-4 me-3">発病負傷経過（あんま・マッサージ）（{{ $counts['conditions'] }}件）</div>
       </a>
     </div>
     <div class="col-12">
       <a href="{{ route('submaster.illnesses-massage') }}" class="btn-ex-main btn-ex-blue btn-ex-rounded-full">
-        <div class="btn-ex-skin btn-ex-white btn-ex-rounded-full aspect-square m-n05">
-          <i class="nf nf-md-clipboard_plus fs-7 m-n3"></i>
+        <div class="btn-ex-skin btn-ex-white btn-ex-xs btn-ex-rounded-full aspect-square m-n05 p-3">
+          <i class="nf nf-md-clipboard_plus fs-6 m-n2"></i>
         </div>
         <div class="fs-5-5 ms-4 me-3">傷病名（あんま・マッサージ）（{{ $counts['illnesses_massage'] }}件）</div>
       </a>
