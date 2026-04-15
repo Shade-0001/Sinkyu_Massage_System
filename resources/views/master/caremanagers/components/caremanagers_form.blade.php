6af3f542d14f36848e4677c82ccc223c360519cb
Shade-0001 <ak74g3@gmail.com>
2026-04-15 16:26:24 +0900

    ［auto-commit_260415-1626｜確認ページ経由フォームのボタンをbtn-ex-greenからbtn-ex-blueに戻す（登録確認へ系はblue）］

--- resources/views/master/caremanagers/components/caremanagers_form.blade.php
+++ resources/views/master/caremanagers/components/caremanagers_form.blade.php
@@ -169,7 +169,7 @@
 
     <div class="mt-4 d-flex gap-2 justify-content-end">
       <a href="{{ $cancelRoute }}" class="btn-ex-main btn-ex-gray"><i class="nf nf-fa-caret_left me-1" style="transform: scale(1.2)"></i>戻る</a>
-      <button type="submit" class="btn-ex-main btn-ex-green">{{ $submitLabel }}</button>
+      <button type="submit" class="btn-ex-main btn-ex-blue">{{ $submitLabel }}</button>
     </div>
 
   </div>
