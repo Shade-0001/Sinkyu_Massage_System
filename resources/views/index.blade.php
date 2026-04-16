dc8467408c66aba4165559c9453a9b379e185698
Shade-0001 <ak74g3@gmail.com>
2026-04-16 12:18:15 +0900

    ［auto-commit_260416-1218｜ホームメニュー：全8要素のフォーマットを統一（btn-ex-main・gap-3・d-flex flex-column・fw-lighter適用）］

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -43,32 +43,42 @@
   <div class="mt-2 mx-1 user-select-none" style="max-width: 1200px;">
     <div id="home-menu" class="d-flex flex-column">
       <div class="flex-fill">
-        <a class="d-flex mb-3 bg-gray-98 rounded-2 border-bevel-light hover-bg-white hover-highlight-20" href="{{ route('records.index') }}">
-          <div class="d-flex align-items-center justify-content-center rounded text-white m-2 me-3 fs-2 flex-shrink-0 border-bevel-3" style="background-color:#E74C3C; aspect-ratio: 1/1;"><i class="nf nf-fa-edit px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i></div>
-          <div>
-            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap mt-3">実績データ</div>
-            <div class="text-gray-30 fw-medium mt-1 mb-3 text-nowrap">施術実績の入力･管理</div>
+
+        <a class="d-flex justify-content-start btn-ex-main bg-gray-98 rounded-2 gap-3" href="{{ route('records.index') }}">
+          <div class="d-flex align-items-center justify-content-center rounded text-white fs-2 flex-shrink-0 border-bevel-3" style="background-color:#E74C3C; aspect-ratio: 1/1;">
+            <i class="nf nf-fa-edit px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i>
+          </div>
+          <div class="d-flex flex-column gap-3">
+            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap fw-lighter">実績データ</div>
+            <div class="text-gray-30 fw-medium text-nowrap">施術実績の入力･管理</div>
           </div>
         </a>
-        <a class="d-flex mb-3 bg-gray-98 rounded-2 border-bevel-light hover-bg-white hover-highlight-20" href="{{ route('reports.index') }}">
-          <div class="d-flex align-items-center justify-content-center rounded text-white m-2 me-3 fs-2 flex-shrink-0 border-bevel-3" style="background-color:#E67E22; aspect-ratio: 1/1;"><i class="nf nf-fa-file_text_o px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i></div>
-          <div>
-            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap mt-3">報告書データ</div>
-            <div class="text-gray-30 fw-medium mt-1 mb-3 text-nowrap">各種報告書の入力･管理</div>
+        
+        <a class="d-flex justify-content-start btn-ex-main bg-gray-98 rounded-2 gap-3" href="{{ route('reports.index') }}">
+          <div class="d-flex align-items-center justify-content-center rounded text-white fs-2 flex-shrink-0 border-bevel-3" style="background-color:#E67E22; aspect-ratio: 1/1;">
+            <i class="nf nf-fa-file_text_o px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i>
+          </div>
+          <div class="d-flex flex-column gap-3">
+            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap fw-lighter">報告書データ</div>
+            <div class="text-gray-30 fw-medium text-nowrap">各種報告書の入力･管理</div>
           </div>
         </a>
-        <a class="d-flex mb-3 bg-gray-98 rounded-2 border-bevel-light hover-bg-white hover-highlight-20" href="{{ route('schedules.index') }}">
-          <div class="d-flex align-items-center justify-content-center rounded text-white m-2 me-3 fs-2 flex-shrink-0 border-bevel-3" style="background-color:#EDBE00; aspect-ratio: 1/1;"><i class="nf nf-md-calendar_month_outline px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i></div>
-          <div>
-            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap mt-3">スケジュール</div>
-            <div class="text-gray-30 fw-medium mt-1 mb-3 text-nowrap">施術スケジュールの確認･管理</div>
+        <a class="d-flex justify-content-start btn-ex-main bg-gray-98 rounded-2 gap-3" href="{{ route('schedules.index') }}">
+          <div class="d-flex align-items-center justify-content-center rounded text-white fs-2 flex-shrink-0 border-bevel-3" style="background-color:#EDBE00; aspect-ratio: 1/1;">
+            <i class="nf nf-md-calendar_month_outline px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i>
+          </div>
+          <div class="d-flex flex-column gap-3">
+            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap fw-lighter">スケジュール</div>
+            <div class="text-gray-30 fw-medium text-nowrap">施術スケジュールの確認･管理</div>
           </div>
         </a>
-        <a class="d-flex mb-3 bg-gray-98 rounded-2 border-bevel-light hover-bg-white hover-highlight-20" href="{{ route('master.index') }}">
-          <div class="d-flex align-items-center justify-content-center rounded text-white m-2 me-3 fs-2 flex-shrink-0 border-bevel-3" style="background-color:#8FDC00; aspect-ratio: 1/1;"><i class="nf nf-oct-gear px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i></div>
-          <div>
-            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap mt-3">マスター登録</div>
-            <div class="text-gray-30 fw-medium mt-1 mb-3 text-nowrap">利用者･医師･施術者などの登録･管理</div>
+        <a class="d-flex justify-content-start btn-ex-main bg-gray-98 rounded-2 gap-3" href="{{ route('master.index') }}">
+          <div class="d-flex align-items-center justify-content-center rounded text-white fs-2 flex-shrink-0 border-bevel-3" style="background-color:#8FDC00; aspect-ratio: 1/1;">
+            <i class="nf nf-oct-gear px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i>
+          </div>
+          <div class="d-flex flex-column gap-3">
+            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap fw-lighter">マスター登録</div>
+            <div class="text-gray-30 fw-medium text-nowrap">利用者･医師･施術者などの登録･管理</div>
           </div>
         </a>
       </div>
@@ -78,32 +88,40 @@
       </div>
 
       <div class="flex-fill">
-        <a class="d-flex mb-3 bg-gray-98 rounded-2 border-bevel-light hover-bg-white hover-highlight-20" href="{{ route('submaster.index') }}">
-          <div class="d-flex align-items-center justify-content-center rounded text-white m-2 me-3 fs-2 flex-shrink-0 border-bevel-3" style="background-color:#1BAD50; aspect-ratio: 1/1;"><i class="nf nf-oct-gear px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i></div>
-          <div>
-            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap mt-3">サブマスター登録</div>
-            <div class="text-gray-30 fw-medium mt-1 mb-3 text-nowrap">医療機関･サービス事業者などの登録･管理</div>
+        <a class="d-flex justify-content-start btn-ex-main bg-gray-98 rounded-2 gap-3" href="{{ route('submaster.index') }}">
+          <div class="d-flex align-items-center justify-content-center rounded text-white fs-2 flex-shrink-0 border-bevel-3" style="background-color:#1BAD50; aspect-ratio: 1/1;">
+            <i class="nf nf-oct-gear px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i>
+          </div>
+          <div class="d-flex flex-column gap-3">
+            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap fw-lighter">サブマスター登録</div>
+            <div class="text-gray-30 fw-medium text-nowrap">医療機関･サービス事業者などの登録･管理</div>
           </div>
         </a>
-        <a class="d-flex mb-3 bg-gray-98 rounded-2 border-bevel-light hover-bg-white hover-highlight-20" href="{{ route('prints.index') }}">
-          <div class="d-flex align-items-center justify-content-center rounded text-white m-2 me-3 fs-2 flex-shrink-0 border-bevel-3" style="background-color:#00CAC0; aspect-ratio: 1/1;"><i class="nf nf-md-printer_outline px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i></div>
-          <div>
-            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap mt-3">印刷メニュー</div>
-            <div class="text-gray-30 fw-medium mt-1 mb-3 text-nowrap">各種文書のPDF出力</div>
+        <a class="d-flex justify-content-start btn-ex-main bg-gray-98 rounded-2 gap-3" href="{{ route('prints.index') }}">
+          <div class="d-flex align-items-center justify-content-center rounded text-white fs-2 flex-shrink-0 border-bevel-3" style="background-color:#00CAC0; aspect-ratio: 1/1;">
+            <i class="nf nf-md-printer_outline px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i>
+          </div>
+          <div class="d-flex flex-column gap-3">
+            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap fw-lighter">印刷メニュー</div>
+            <div class="text-gray-30 fw-medium text-nowrap">各種文書のPDF出力</div>
           </div>
         </a>
-        <a class="d-flex mb-3 bg-gray-98 rounded-2 border-bevel-light hover-bg-white hover-highlight-20" href="{{ route('therapy-periods.index') }}">
-          <div class="d-flex align-items-center justify-content-center rounded text-white m-2 me-3 fs-2 flex-shrink-0 border-bevel-3" style="background-color:#1F91CE; aspect-ratio: 1/1;"><i class="nf nf-fa-list px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i></div>
-          <div>
-            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap mt-3">要加療期間リスト</div>
-            <div class="text-gray-30 fw-medium mt-1 mb-3 text-nowrap">要加療期間の確認･管理</div>
+        <a class="d-flex justify-content-start btn-ex-main bg-gray-98 rounded-2 gap-3" href="{{ route('therapy-periods.index') }}">
+          <div class="d-flex align-items-center justify-content-center rounded text-white fs-2 flex-shrink-0 border-bevel-3" style="background-color:#1F91CE; aspect-ratio: 1/1;">
+            <i class="nf nf-fa-list px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i>
+          </div>
+          <div class="d-flex flex-column gap-3">
+            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap fw-lighter">要加療期間リスト</div>
+            <div class="text-gray-30 fw-medium text-nowrap">要加療期間の確認･管理</div>
           </div>
         </a>
-        <a class="d-flex mb-3 bg-gray-98 rounded-2 border-bevel-light hover-bg-white hover-highlight-20" href="{{ route('deposits.index') }}">
-          <div class="d-flex align-items-center justify-content-center rounded text-white m-2 me-3 fs-2 flex-shrink-0 border-bevel-3" style="background-color:#B164E4; aspect-ratio: 1/1;"><i class="nf nf-fa-yen px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i></div>
-          <div>
-            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap mt-3">入金管理</div>
-            <div class="text-gray-30 fw-medium mt-1 mb-3 text-nowrap">入金情報の入力･管理</div>
+        <a class="d-flex justify-content-start btn-ex-main bg-gray-98 rounded-2 gap-3" href="{{ route('deposits.index') }}">
+          <div class="d-flex align-items-center justify-content-center rounded text-white fs-2 flex-shrink-0 border-bevel-3" style="background-color:#B164E4; aspect-ratio: 1/1;">
+            <i class="nf nf-fa-yen px-4 text-gray-95 highlight-target-text-white" style="font-size: 3rem;"></i>
+          </div>
+          <div class="d-flex flex-column gap-3">
+            <div class="home-menu-label text-gray-95 highlight-target-text-white text-nowrap fw-lighter">入金管理</div>
+            <div class="text-gray-30 fw-medium text-nowrap">入金情報の入力･管理</div>
           </div>
         </a>
       </div>
