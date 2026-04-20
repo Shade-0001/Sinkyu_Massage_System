be07e6349fbaba46d14fb72c5af6b70f832d3a2b
Shade-0001 <ak74g3@gmail.com>
2026-04-20 13:59:44 +0900

    ［auto-commit_260420-1359｜documents_form：行間隔フィールドのstepを0.01→0.1に変更（小数点1桁まで）］

--- resources/views/master/documents/components/documents_form.blade.php
+++ resources/views/master/documents/components/documents_form.blade.php
@@ -103,7 +103,7 @@
     </label>
     <div class="form-field px-3 py-2">
       <div class="form-field-top"></div>
-      <input type="number" name="line_height" id="line_height" value="{{ old('line_height', $item->line_height ?? 7) }}" step="0.01" style="width: 100px;">
+      <input type="number" name="line_height" id="line_height" value="{{ old('line_height', $item->line_height ?? 7) }}" step="0.1" style="width: 100px;">
     </div>
   </div>
 
