9a0f9827be344f86cf8a9faff0eb7512d261227c
Shade-0001 <ak74g3@gmail.com>
2026-04-23 14:39:10 +0900

    ［auto-commit_260423-1439｜redirectUsersToを/indexから/homeに修正（ログイン済みユーザーが/indexで404になる問題を修正）］

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo('/login');
-        $middleware->redirectUsersTo('/index');
+        $middleware->redirectUsersTo('/home');
 
 
         // セッション有効性チェックミドルウェアをwebグループに追加
